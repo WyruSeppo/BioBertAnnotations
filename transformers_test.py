8d5294baf88a29fc76e2e6bb19488a06a8be4c42
WyruSeppo <52449201+WyruSeppo@users.noreply.github.com>
2024-11-06 18:31:08 +0100

    Add files via upload

---
+++ transformers_test.py
@@ -0,0 +1,11 @@
+<<<<<<< HEAD
+from transformers import pipeline
+
+classifier = pipeline("sentiment-analysis")
+print(classifier(""))
+=======
+from transformers import pipeline
+
+classifier = pipeline("sentiment-analysis")
+print(classifier(""))
+>>>>>>> 49b9adc1 (init)
