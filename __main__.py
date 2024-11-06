384573d99672313aa5dd1155ad1c61317c8a166c
Sebastian Rossboeck <sebastian.rossboeck@centauri.at>
2024-11-06 18:33:57 +0100

    update

--- __main__.py
+++ __main__.py
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 from dataMethods import *
 
 tsv_file_path = "spore_formers_proteinIds"
 annotated_df = annotate_tsv(tsv_file_path)
-=======
-from dataMethods import *
-
-tsv_file_path = "spore_formers_proteinIds"
-annotated_df = annotate_tsv(tsv_file_path)
->>>>>>> 49b9adc1 (init)
 annotated_df.to_csv('output.txt', sep='\t', index=False)
--- transformers_test.py
+++ transformers_test.py
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 from transformers import pipeline
 
 classifier = pipeline("sentiment-analysis")
 print(classifier(""))
-=======
-from transformers import pipeline
-
-classifier = pipeline("sentiment-analysis")
-print(classifier(""))
->>>>>>> 49b9adc1 (init)
